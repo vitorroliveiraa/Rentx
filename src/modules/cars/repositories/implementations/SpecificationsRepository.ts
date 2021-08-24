99822b929ef7da6e2e34b10c4838b2a63393596c
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2021-08-24 17:54:30 -0300

    useCase Specification criada

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,14 +1,23 @@
-import { Specification } from "../model/Specification";
+import { Specification } from "../../model/Specification";
 import {
     ISpecificationsRepository,
     ICreateSpecificationDTO,
-} from "./ISpecificationsRepository";
+} from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
     private specifications: Specification[];
 
-    constructor() {
+    private static INSTANCE: SpecificationsRepository;
+
+    private constructor() {
         this.specifications = [];
+    }
+
+    public static getInstance(): SpecificationsRepository {
+        if (!SpecificationsRepository.INSTANCE) {
+            SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+        }
+        return SpecificationsRepository.INSTANCE;
     }
 
     findByName(name: string): Specification {
