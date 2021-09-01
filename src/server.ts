f12b56c3e0b57ab0b2de26e7365427d5cd6d3df5
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2021-09-01 12:14:42 -0300

    Criação da documentação

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 // Quando o path termina com nome de pasta, automaticamente pega index
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
