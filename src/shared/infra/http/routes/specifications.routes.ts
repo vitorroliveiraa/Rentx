3ca691702c4967dae6998248e1cfee9c57fd4174
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-03-09 19:34:36 -0300

    feat: adding validations to routes: cars, categories and specs (ensureAuthenticated and ensureAdmin)

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
 
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
-
-import { ensureAuthenticated } from '../middlewares/ensureAuthenticate';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticate';
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post(
+    '/',
+    ensureAuthenticated,
+    ensureAdmin,
+    createSpecificationController.handle
+);
 
 export { specificationsRoutes };
