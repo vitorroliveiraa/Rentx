c5c3ea2265900d76e3b25ee9923ce6a7f12394e8
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-03-10 19:17:03 -0300

    feat: change in the search condition of available cars

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -43,10 +43,10 @@ class CarsRepositoryInMemory implements ICarsRepository {
     ): Promise<Car[]> {
         const cars = this.cars.filter((car) => {
             if (
-                car.available === true &&
-                ((brand && car.brand === brand) ||
-                    (category_id && car.category_id === category_id) ||
-                    (name && car.name === name))
+                car.available === true ||
+                (brand && car.brand === brand) ||
+                (category_id && car.category_id === category_id) ||
+                (name && car.name === name)
             ) {
                 return car;
             }
