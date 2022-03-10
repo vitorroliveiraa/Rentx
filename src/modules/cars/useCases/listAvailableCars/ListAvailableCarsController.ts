7b1c8fdffcc5f72dbb04eab03003d6526b90bbbe
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-03-10 19:17:45 -0300

    feat: adding the cars route controller

---
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ListAvailableCarsUseCase } from './ListAvailableCarsUseCase';
+
+class ListAvailableCarsController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { category_id, brand, name } = request.query;
+
+        const listAvailableCarsUseCase = container.resolve(
+            ListAvailableCarsUseCase
+        );
+
+        // ? É uma forma de garantir que o valor enviado vai ser uma string
+        const cars = await listAvailableCarsUseCase.execute({
+            category_id: category_id as string,
+            brand: brand as string,
+            name: name as string,
+        });
+
+        return response.json(cars);
+    }
+}
+
+export { ListAvailableCarsController };
