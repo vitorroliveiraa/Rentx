220098e16fd0cc05b1462220dc288f667aff3450
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-03-21 15:47:40 -0300

    feat: add conditions in connection database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,12 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (host = 'localhost'): Promise<Connection> => {
-    const defaultOptions = await getConnectionOptions();
+  const defaultOptions = await getConnectionOptions();
 
-    return createConnection(
-        Object.assign(defaultOptions, {
-            host,
-        })
-    );
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      database: process.env.NODE_ENV === 'test' ? '' : defaultOptions.database,
+    })
+  );
 };
