b2a42d2d7122ee409e91cd9503a5fdb8bc0d36c8
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-03-21 17:24:58 -0300

    refactor: category import fix

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,21 +1,20 @@
 import { inject, injectable } from 'tsyringe';
 
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
-
-import { Category } from '../../entities/Category';
 
 @injectable()
 class ListCategoriesUseCase {
-    constructor(
-        @inject('CategoriesRepository')
-        private categoriesRepository: ICategoriesRepository
-    ) {}
+  constructor(
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository
+  ) {}
 
-    async execute(): Promise<Category[]> {
-        const categories = await this.categoriesRepository.list();
+  async execute(): Promise<Category[]> {
+    const categories = await this.categoriesRepository.list();
 
-        return categories;
-    }
+    return categories;
+  }
 }
 
 export { ListCategoriesUseCase };
