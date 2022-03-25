fb9fa7f174551e4dac786d7766277ca94d32ea94
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-03-25 19:31:19 -0300

    test: first integration tests

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -8,9 +8,10 @@ import createConnection from '@shared/infra/typeorm';
 
 let connection: Connection;
 
-describe('Create Category Controller', () => {
+describe('List Categories Controller', () => {
   beforeAll(async () => {
     connection = await createConnection();
+
     await connection.runMigrations();
 
     const id = uuidV4();
@@ -28,7 +29,7 @@ describe('Create Category Controller', () => {
     await connection.close();
   });
 
-  it('should be able to create a new  category', async () => {
+  it('should be able to list all categories', async () => {
     const responseToken = await request(app).post('/sessions').send({
       email: 'admin@rentx.com.br',
       password: 'admin',
@@ -36,7 +37,7 @@ describe('Create Category Controller', () => {
 
     const { token } = responseToken.body;
 
-    const response = await request(app)
+    const cat = await request(app)
       .post('/categories')
       .send({
         name: 'Category Supertest',
@@ -46,27 +47,10 @@ describe('Create Category Controller', () => {
         Authorization: `Bearer ${token}`,
       });
 
-    expect(response.status).toBe(201);
-  });
+    const response = await request(app).get('/categories');
 
-  it('should not be able to create a new category with name exists', async () => {
-    const responseToken = await request(app).post('/sessions').send({
-      email: 'admin@rentx.com.br',
-      password: 'admin',
-    });
-
-    const { token } = responseToken.body;
-
-    const response = await request(app)
-      .post('/categories')
-      .send({
-        name: 'Category Supertest',
-        description: 'Category Supertest',
-      })
-      .set({
-        Authorization: `Bearer ${token}`,
-      });
-
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toHaveProperty('id');
   });
 });
