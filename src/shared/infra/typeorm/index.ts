e46d0da4e49212a9c04f79e4946502e177633bf7
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-03-25 19:33:31 -0300

    feat: database for tests added in environment variable

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,15 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'localhost'): Promise<Connection> => {
+export default async (host = 'database'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
       host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
-      database: process.env.NODE_ENV === 'test' ? '' : defaultOptions.database,
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'rentx_test'
+          : defaultOptions.database,
     })
   );
 };
