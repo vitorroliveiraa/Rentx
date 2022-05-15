6738d55fc0d3903ef1865e01b84a8830e141eef4
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-05-15 19:42:05 -0300

    feat: creating a method to return the avatar link

--- src/modules/accounts/infra/typeorm/entities/User.ts
+++ src/modules/accounts/infra/typeorm/entities/User.ts
@@ -1,37 +1,51 @@
+import { Expose } from 'class-transformer';
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
 @Entity('users')
 class User {
-    @PrimaryColumn()
-    id: string;
+  @PrimaryColumn()
+  id: string;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @Column()
-    driver_license: string;
+  @Column()
+  driver_license: string;
 
-    @Column()
-    isAdmin: boolean;
+  @Column()
+  isAdmin: boolean;
 
-    @Column()
-    avatar: string;
+  @Column()
+  avatar: string;
 
-    @CreateDateColumn()
-    created_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
-    constructor() {
-        if (!this.id) {
-            this.id = uuidV4();
-        }
+  @Expose({ name: 'avatar_url' })
+  avatar_url(): string {
+    switch (process.env.disk) {
+      case 'local':
+        return `${process.env.API_URL}/avatar/${this.avatar}`;
+      case 's3':
+        return `${process.env.AWS_BUCKET_URL}/avatar/${this.avatar}`;
+
+      default:
+        return null;
     }
+  }
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
 }
 
 export { User };
